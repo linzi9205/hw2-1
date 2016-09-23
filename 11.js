9caf228c277cea4f8a461dabf8249508b75e0639
linzi9205 <lwu@cca.edu>
2016-09-22 22:59:20 -0700

    Create 11.js

---
+++ 11.js
@@ -0,0 +1,21 @@
+var a=0;
+
+function setup() {
+  createCanvas(600,600);
+  background(0);
+  frameRate(10);
+
+}
+
+function draw() {
+    strokeWeight(3);
+    var l=random(600);
+    stroke(255);
+    line(a,600,a,l);
+    a=a+20;
+    if (a>600){
+      a=0;
+    }
+       stroke(0);
+      line (a,600,a,0);
+}
