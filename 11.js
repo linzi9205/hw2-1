c22f50d28bc6f1bc22c7344ea2e30e3070bd611d
linzi9205 <lwu@cca.edu>
2016-09-22 23:03:47 -0700

    Update 11.js

--- 11.js
+++ 11.js
@@ -10,12 +10,12 @@ function setup() {
 function draw() {
     strokeWeight(3);
     var l=random(600);
+    stroke(0);
+    line (a,600,a,0);
     stroke(255);
     line(a,600,a,l);
     a=a+20;
     if (a>600){
       a=0;
     }
-       stroke(0);
-      line (a,600,a,0);
 }
