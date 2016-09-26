c43f169b916bf064d4c990b3be3765020f349512
linzi9205 <lwu@cca.edu>
2016-09-26 10:44:15 -0700

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,3 +1,5 @@
+// FIRST EDITION:
+
 function setup() {
   createCanvas(850,850);
   background(0);
@@ -8,6 +10,28 @@ function setup() {
     noStroke();
     fill(360*(1.8*a/35+55*b/35)/1640,100,100);
     ellipse(a,b,30,30);
+    }
+  }
+}
+function draw() {
+}
+
+
+
+// SECOND EDITION:
+
+function setup() {
+  createCanvas(850,850);
+  background(0);
+  colorMode(HSB);
+  var h=10;
+  noStroke();
+  
+  for(var y=40;y<820;y=y+35){
+    for(var x=40;x<820;x=x+35){
+    fill(h,100,100);
+    h=h+0.5;
+    ellipse(x,y,30,30);
     }
   }
 }
