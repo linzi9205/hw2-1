0825aec1e9abc7178ec7aee38e4f18fab6d12e18
linzi9205 <lwu@cca.edu>
2016-09-26 08:48:53 -0700

    Update 4.js

--- 4.js
+++ 4.js
@@ -6,8 +6,7 @@ function setup() {
   for(a=40;a<820;a=a+35){
     for(b=40;b<820;b=b+35){
     noStroke();
-    var h=random(360);
-    fill(360*(a+b)/1640,100,100);
+    fill(360*(1.8*a/35+55*b/35)/1640,100,100);
     ellipse(a,b,30,30);
     }
   }
